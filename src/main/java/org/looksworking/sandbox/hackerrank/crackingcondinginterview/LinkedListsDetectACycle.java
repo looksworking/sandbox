db9c4aa052fd6a8605c584437fc1fee09b660aa8
Vyacheslav Petrukhin <petrukhinv@gmail.com>
2017-09-03 21:44:55 +0300

    Hackerrank: detect a cycle

---
+++ src/main/java/org/looksworking/sandbox/hackerrank/crackingcondinginterview/LinkedListsDetectACycle.java
@@ -0,0 +1,54 @@
+package org.looksworking.sandbox.hackerrank.crackingcondinginterview;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class LinkedListsDetectACycle {
+
+    @Test
+    public void LinkedListsDetectACycleTest() {
+
+        Node node1 = new Node();
+        Node node2 = new Node();
+        Node node3 = new Node();
+
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node1;
+
+        boolean expected = true;
+        boolean actual = hasCycle(node1);
+
+        assertEquals(expected, actual);
+
+    }
+
+    boolean hasCycle(Node head) {
+
+        boolean result = false;
+
+        List<Node> nodes = new ArrayList<>();
+
+        while (head != null && head.next != null) {
+
+            nodes.add(head);
+
+            if (nodes.contains(head.next)) {
+                return true;
+            }
+
+            head = head.next;
+        }
+
+        return result;
+    }
+}
+
+class Node {
+    int data;
+    Node next;
+}
