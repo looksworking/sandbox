0f133a8a382f23485da9a6a0c6e0ddbe91f7a151
Vyacheslav Petrukhin <petrukhinv@gmail.com>
2017-09-10 10:45:23 +0300

    Hackerrank: is this a binary search tree

--- src/main/java/org/looksworking/sandbox/hackerrank/crackingcondinginterview/TreesIsThisABinarySearchTree.java
+++ src/main/java/org/looksworking/sandbox/hackerrank/crackingcondinginterview/TreesIsThisABinarySearchTree.java
@@ -4,7 +4,7 @@ package org.looksworking.sandbox.hackerrank.crackingcondinginterview;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class TreesIsThisABinarySearchTree {
 
